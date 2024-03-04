093846d3c4d65ab538e7db075c24e462c15c01c1
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-04 16:20:08 +0100

    feat: add the stamp in the HTML page with get element by id

--- script.js
+++ script.js
@@ -5,21 +5,22 @@ alert('Creiamo la tua password in 3 step');
 // Chiedere all'utente il suo nome 
 
 const yourName = prompt ('1. Scrivi il tuo nome');
-console.log(yourName);
 
 // Chiedere all'utente il suo cognome
 
 const yourSurname = prompt ('2. Scrivi il tuo cognome');
-console.log(yourSurname);
 
 // Chiedere all'utente il suo colore preferito
 
 const yourFavColor = prompt ('3. Scrivi il tuo colore preferito');
-console.log(yourFavColor);
 
 // Concatenare tutte le stringhe che ci fornisce l'utente
 
-const yourPassword = yourName + yourSurname + yourFavColor + 23;
-console.log(yourPassword);
+const yourPassword = yourName + yourSurname + yourFavColor + '23';
+
+// Creaiamo la variabile const del messaggio da mostrare
+
+const resultPassword = 'Questa è la tua password: ' + yourPassword;
 
 // Stampare sulla pagina HTML la password
+document.getElementById('password').innerHTML = resultPassword
