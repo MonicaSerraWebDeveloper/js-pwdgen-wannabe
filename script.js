69c6a397f225f503942b0bec8bb82ae19c709c75
MonicaSerraWebDeveloper <monica.serra.webdeveloper@gmail.com>
2024-03-04 16:06:24 +0100

    feat: add an alert at the beggining to make the things more real

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+// Alert per avvisare l'utente che stiamo per creare la sua password NON sicura
+
+alert('Creiamo la tua password in 3 step');
+
 // Chiedere all'utente il suo nome 
 
-const yourName = prompt ('Ciao, scrivi il tuo nome');
-console.log(yourName)
+const yourName = prompt ('1. Scrivi il tuo nome');
+console.log(yourName);
 
 // Chiedere all'utente il suo cognome
+
+const yourSurname = prompt ('2. Scrivi il tuo cognome');
+console.log(yourSurname);
 
 // Chiedere all'utente il suo colore preferito
 
